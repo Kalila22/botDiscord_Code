c8c57f24641a63e677d1bcdb0dad0d512a94f551
Kalila22 <luthfiaazka.kalila@gmail.com>
2026-07-03 21:17:58 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,4 +28,52 @@ async def choose(ctx, *choices: str):
 async def cool(ctx):
     await ctx.send('Yes, the bot is cool.')
 
+@bot.command()
+async def meme(ctx):
+    images = os.listdir("images(MODUL 2)")
+
+    img_name = random.choice(images)
+
+    with open("images(MODUL 2)/" + img_name, "rb") as f:
+        picture = discord.File(f)
+
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_fox_image():
+    url = "https://randomfox.ca/floof/"
+    res = requests.get(url)
+    data = res.json()
+    return data["image"]
+
+@bot.command()
+async def fox(ctx):
+    await ctx.send(get_fox_image())
+
+@bot.command()
+async def fakta(ctx):
+    await ctx.send(get_fakta())
+
+@bot.command()
+async def tips(ctx):
+    await ctx.send(get_tips())
+
+@bot.command()
+async def recycle(ctx):
+    await ctx.send(get_recycle())
+
+@bot.command()
+async def laut(ctx):
+    await ctx.send(get_laut())
+
 bot.run("TOKEN")
